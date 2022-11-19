9be6d645f2dd325779fddd5b2a44823405081665
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2022-11-19 23:39:00 +0100

    refactor: marker

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,85 +1,73 @@
 import { useEffect, useState } from "react";
 import Map, {
-  Source,
-  Layer,
-  LayerProps,
+  Marker,
   FullscreenControl,
   NavigationControl,
-  GeolocateControl
+  GeolocateControl,
 } from "react-map-gl";
 import { Position } from "geojson";
 import { useGetAllMarkersQuery } from "../../features/api/mapSlice";
 import { useGeoLocation } from "../../hooks/useGeoLocation";
 
-const layerStyle = {
-  id: "point",
-  type: "circle",
-  paint: {
-    "circle-radius": 15,
-    "circle-color": "#007cbf",
-  },
-} as LayerProps;
-
-const initialFeatureCollection: GeoJSON.FeatureCollection = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [] as Position,
-      },
-      properties: { title: "t" },
+const initialFeatureCollection: any = [
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [] as Position,
     },
-  ],
-};
+    properties: {
+      image: "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+    },
+  },
+];
 
 const MapComponent = () => {
-  const [featureCollection, setFeatureCollection] =
-    useState<GeoJSON.FeatureCollection>(initialFeatureCollection);
+  const [featureCollection, setFeatureCollection] = useState<any>(
+    initialFeatureCollection
+  );
 
   const { lat, lng, error: errGeoLoc } = useGeoLocation();
-
   const { data, error, isLoading } = useGetAllMarkersQuery(null);
 
   useEffect(() => {
     if (data) {
-      setFeatureCollection((prev) => {
-        return {
-          type: "FeatureCollection",
-          features: [
-            ...prev.features,
-            {
-              type: "Feature",
-              geometry: {
-                type: "Point",
-                coordinates: [data.longitude, data.latitude],
-              },
-              properties: { title: data.city },
+      setFeatureCollection((prev: any) => {
+        return [
+          ...prev,
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [data.longitude, data.latitude],
             },
-          ],
-        };
+            properties: {
+              image:
+                "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+            },
+          },
+        ];
       });
     }
   }, [data]);
 
   useEffect(() => {
     if (lat && lng) {
-      setFeatureCollection((prev) => {
-        return {
-          type: "FeatureCollection",
-          features: [
-            ...prev.features,
-            {
-              type: "Feature",
-              geometry: {
-                type: "Point",
-                coordinates: [lng, lat],
-              },
-              properties: { title: "You are here" },
+      setFeatureCollection((prev: any) => {
+        return [
+          ...prev,
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [lng, lat],
             },
-          ],
-        };
+            properties: {
+              image:
+                "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+            },
+          },
+        ];
       });
     }
   }, [lat, lng]);
@@ -97,11 +85,35 @@ const MapComponent = () => {
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
         <FullscreenControl />
+
         <NavigationControl />
         <GeolocateControl />
-        <Source id="my-data" type="geojson" data={featureCollection}>
-          <Layer {...layerStyle} />
-        </Source>
+        {featureCollection.map((feature: any, index: any) => {
+          const {
+            coordinates: [markerLat, markerLng],
+          } = feature.geometry;
+          const { title, image } = feature.properties;
+          return (
+            markerLat &&
+            markerLng && (
+              <Marker
+                longitude={feature.geometry.coordinates[0]}
+                latitude={feature.geometry.coordinates[1]}
+                anchor="bottom"
+                key={index}
+                style={{ display: "grid", placeItems: "center" }}
+              >
+                <img src={image} alt="title" />
+                <span
+                  style={{
+                    maxWidth: "90px",
+                    textAlign: "center",
+                  }}
+                ></span>
+              </Marker>
+            )
+          );
+        })}
       </Map>
     )
   );
